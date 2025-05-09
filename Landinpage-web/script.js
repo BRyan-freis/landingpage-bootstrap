8bbb70329a39f89f0d6f59ee418a35697702c689
Bryan <bryanferreiradosreisferreira@gmail.com>
2025-05-09 17:56:29 -0300

    Add files via upload

--- Landinpage-web/script.js
+++ Landinpage-web/script.js
@@ -5,3 +5,26 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
 })
+
+const video = document.getElementById('narracao');
+        const audioBtn = document.getElementById('audio-btn');
+
+        // Permite ativar o som
+        audioBtn.addEventListener('click', () => {
+            video.muted = false;
+            video.play();
+        });
+
+        // Quando o vídeo termina, mostra controles
+        video.addEventListener('ended', () => {
+            video.controls = true;
+        });
+
+        // Ao clicar no vídeo, reinicia
+        video.addEventListener('click', () => {
+            if (video.paused || video.ended) {
+                video.currentTime = 0;
+                video.play();
+                video.controls = false;
+            }
+        });
